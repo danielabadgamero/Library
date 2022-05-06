95c9412db377761906d964d26d35b1837a49dd12
danielabadgamero <danielabadgamero@gmail.com>
2022-05-06 17:44:47 +0200

    Adding add book event listener

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const addBook = document.getElementsByClassName("add-book");
+const bookAddCard = document.getElementsByClassName("add-book");
+const addForm = document.getElementsByClassName("add-form");
+
+function addBook(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+bookAddCard.addEventListener('click', () => {
+    addForm.classList.add("adding-form");
+});
